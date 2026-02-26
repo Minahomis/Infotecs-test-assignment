9f01ca6f2e13e193bdbef2dfe17a2c330c2fa6a3
basiliac <alyacherkova82@gmail.com>
2026-02-26 20:23:54 +0700

    build: add started.sh for build
    started.sh: check for docker image

--- started.sh
+++ started.sh
@@ -5,6 +5,11 @@
 
 if sudo docker image inspect nginx > /dev/null 2>&1; then
     echo "Образ существует"
+    $(sudo docker create nginx)
 else 
     echo "Такого образа не существует"
+    $(sudo docker build nginx)
+    $(sudo docker create nginx)
 fi
+
+
